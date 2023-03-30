1ddd393ef602f48a4a26aa0415683b42d9f12a36
Priyanka Khare <75668973+PriyankaKhare8@users.noreply.github.com>
2023-03-30 13:33:50 +0530

    Add files via upload
    Include index.js and package file

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var express = require('express');  //Creates instance of expres js
+var app = express();
+
+//Creating the server at port : 33001
+app.listen(3001, function () {
+   console.log("Example app listening at port 3001");
+})
